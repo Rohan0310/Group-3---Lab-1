f0bf3fe1eaab6f7aabb764d1c8b2ffcb941ddd57
npatel0202 <npatel16@confederationcollege.ca>
2021-01-26 15:38:43 -0500

    Step 3 Lab 1 Project 2

--- Project 2/js/data.js
+++ Project 2/js/data.js
@@ -1,5 +1,5 @@
 /* define your array of object literals here */
-var countries =[["Canada","North America", ["Calgary", "Montreal", "Toronto"],["canada1.jpg", "canada2.jpg", "canada3.jpg"]],
+var countries =[["Canada","North America",["Calgary", "Montreal", "Toronto"],["canada1.jpg", "canada2.jpg", "canada3.jpg"]],
 ["United States","North America", ["Boston","Chicago","New York","Seattle","Washington"],["us1.jpg","us2.jpg"]],
 ["Italy","Europe",["Florence","Milan","Naples","Rome"],["italy1.jpg","italy2.jpg","italy3.jpg","italy4.jpg","italy5.jpg","italy6.jpg"]],
-["Spain","Europe",["Almeria","Barcelona","Madrid"],["spain1.jpg","spain2.jpg"]]]
+["Spain","Europe",["Almeria","Barcelona","Madrid"],["spain1.jpg","spain2.jpg"]]];
--- Project 2/js/functions.js
+++ Project 2/js/functions.js
@@ -1,1 +1,16 @@
 /* add in your functions here */
+document.write('<header>'+ '<h2>Share Your Travel</h2></header>'+'<div class="flex-container justify">');
+
+
+function outputcountrybox(name,continent,cities,photos)
+{
+		document.write('<div class="item">'+'<h2>'+ name+'</h2>');
+		document.write('<p>'+ continent +'</p> </div>');
+}
+
+for(let i=0; i<countries.length; i++)
+		{
+			outputcountrybox(countries[i][0], countries[i][1], countries[i][2], countries[i][3]);
+		}
+
+document.write('</div>');
